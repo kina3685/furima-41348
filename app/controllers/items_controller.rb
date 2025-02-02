2468041f6d67209a11616c43cb11c80aadda3fe0
kina3685 <naoto_0507@icloud.com>
2025-02-02 13:34:32 +0900

    商品一覧表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   def index
+    @items = Item.all
   end
 
   def create
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  # has_one :order
+  # def sold_out?
+  # order.present?
+  # end
   # ActiveHash関連
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
