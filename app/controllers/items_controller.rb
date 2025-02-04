a845902617f91923189cab414a10416a3b39f018
kina3685 <naoto_0507@icloud.com>
2025-02-04 16:09:22 +0900

    編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  # before_action :set_item, only: [:edit] # , :destroy
-  # before_action :redirect_if_not_owner, only: [:edit, :update]
+  before_action :set_item, only: [:update, :edit] # , :destroy
+  before_action :redirect_if_not_owner, only: [:edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -33,11 +33,11 @@ class ItemsController < ApplicationController
   end
 
   def update
-    # if @item.update(item_params)
-    #  redirect_to item_path(@item), notice: '商品情報が更新されました。'
-    # else
-    #  render :edit, status: :unprocessable_entity
-    # end
+    if @item.update(item_params)
+      redirect_to item_path(@item), notice: '商品情報が更新されました。'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -55,9 +55,9 @@ class ItemsController < ApplicationController
     redirect_to root_path, alert: '商品が見つかりません。' if @item.nil?
   end
 
-  # def redirect_if_not_owner
-  #    redirect_to root_path, alert: '他のユーザーの商品は編集できません。' if @item.user_id != current_user.id
-  # end
+  def redirect_if_not_owner
+    redirect_to root_path, alert: '他のユーザーの商品は編集できません。' if @item.user_id != current_user.id
+  end
 
   def item_params
     params.require(:item).permit(
